59784a2acd78400e08c4b9a4122195fc55e4292a
Clark Eagling - ee33 <37630289+ClarkEagling@users.noreply.github.com>
2022-01-24 09:51:50 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,121 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "gradient":return img`
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+1111111111111111
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+dddddddddddddddd
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbb
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaa
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+cccccccccccccccc
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+ffffffffffffffff
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,25 +1,103 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-    game.splash("nice!")
+    scene.setBackgroundColor(5)
     info.changeScoreBy(1000)
-    music.jumpUp.play()
+    music.beamUp.play()
+    game.splash("nice!")
     MakeLevel()
 })
+function MakePlatzBar () {
+    platzBar = statusbars.create(20, 4, StatusBarKind.Energy)
+    platzBar.setLabel("platz")
+    platzBar.positionDirection(CollisionDirection.Top)
+    platzBar.setOffsetPadding(-55, 8)
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = -255
-        music.pewPew.play()
+        mySprite.vy = -280
+        mySprite.startEffect(effects.trail, 150)
+        music.thump.play()
     }
 })
+function MakeVertPosBar () {
+    vertBar = statusbars.create(20, 4, StatusBarKind.Health)
+    vertBar.positionDirection(CollisionDirection.Bottom)
+    vertBar.setBarSize(0, 4)
+    vertBar.setOffsetPadding(0, 3)
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     mySprite.destroy()
     game.over(false, effects.splatter)
 })
+function MakeMySprite () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 7 7 . . . . . . 
+        . . . . . . 7 7 7 7 7 7 7 . . . 
+        . . . . . 7 7 1 1 f 7 7 7 . . . 
+        . . . . 7 7 7 1 1 1 7 7 7 7 . . 
+        . . . . 7 f 7 7 7 7 7 7 7 7 . . 
+        . . . . 7 f f f f f f f f 7 7 . 
+        . . . . 7 7 7 2 2 7 7 7 7 . . . 
+        . . . 7 7 7 7 7 7 7 . . . . . . 
+        . . 7 7 7 7 7 7 7 7 7 . . . . . 
+        . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+        . . 7 7 . . . 7 7 7 7 . 7 7 7 . 
+        . . . 7 7 . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 7 7 7 7 7 7 . . 
+        . . . . . 7 7 7 7 7 1 f 7 7 7 . 
+        . . . . 7 7 7 7 7 7 1 1 7 7 7 . 
+        . . . . 7 6 7 7 7 7 7 7 7 7 . . 
+        . . . 7 7 7 7 7 7 7 2 7 7 7 7 7 
+        . . . 7 7 7 7 7 7 7 2 2 f f f 7 
+        . . 7 7 7 7 7 6 7 7 7 7 7 7 7 7 
+        . . 7 7 6 7 7 7 7 7 7 7 7 7 . . 
+        . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+        . . 7 7 7 7 7 7 7 7 7 . . 7 . . 
+        . . 7 7 7 7 7 7 7 7 7 . . 7 7 . 
+        . . 7 7 7 7 7 7 7 7 7 . . . . . 
+        . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 7 7 7 . . . . 7 7 7 . . 
+        . . . . . . . . . . . . . 7 7 . 
+        `,img`
+        . . . . . . . 7 7 7 7 7 7 7 . . 
+        . . . . . 7 7 7 7 7 1 f 7 7 7 . 
+        . . . . 7 7 7 7 7 7 1 1 7 7 7 . 
+        . . . . 7 6 7 7 7 7 7 7 7 7 . . 
+        . . . 7 7 7 7 7 7 7 2 7 7 7 7 7 
+        . . . 7 7 7 7 7 7 7 2 2 f f f 7 
+        . . 7 7 7 7 7 6 7 7 7 7 7 7 7 7 
+        . . 7 7 6 7 7 7 7 7 7 7 7 7 . . 
+        . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+        . . 7 7 7 7 7 7 7 7 7 . . 7 . . 
+        . . 7 7 7 7 7 7 7 7 7 . . 7 7 . 
+        . . 7 7 7 7 7 7 7 7 7 . . . . . 
+        . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . 7 7 7 7 . . . . 7 7 7 . . 
+        . . . . . . . . . . . 7 7 7 . . 
+        . . . . . . . . . . . . . 7 7 . 
+        `],
+    333,
+    true
+    )
+    controller.moveSprite(mySprite, 120, 0)
+    mySprite.ay = 500
+    scene.cameraFollowSprite(mySprite)
+}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (playerPlatforms < 3) {
         tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), sprites.builtin.oceanDepths1)
         tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), true)
         music.knock.play()
         playerPlatforms += 1
+        platzBar.value += -33
     }
 })
 function MakeLevel () {
@@ -38,69 +116,99 @@ function MakeLevel () {
 let colRandom = 0
 let rowRandom = 0
 let platformCount = 0
+let vertBar: StatusBarSprite = null
 let mySprite: Sprite = null
+let platzBar: StatusBarSprite = null
 let playerPlatforms = 0
 let levelNumber = 0
 playerPlatforms = 0
-mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 7 7 7 . . . . . . 
-    . . . . . . 7 7 7 7 7 7 7 . . . 
-    . . . . . 7 7 1 1 f 7 7 7 . . . 
-    . . . . 7 7 7 1 1 1 7 7 7 7 . . 
-    . . . . 7 f 7 7 7 7 7 7 7 7 . . 
-    . . . . 7 f f f f f f f f 7 7 . 
-    . . . . 7 7 7 2 2 7 7 7 7 . . . 
-    . . . 7 7 7 7 7 7 7 . . . . . . 
-    . . 7 7 7 7 7 7 7 7 7 . . . . . 
-    . . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
-    . . 7 7 . . . 7 7 7 7 . 7 7 7 . 
-    . . . 7 7 . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-animation.runImageAnimation(
-mySprite,
-[img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 7 7 7 7 7 7 7 . . 
-    . . . . . 7 7 7 7 7 1 f 7 7 7 . 
-    . . . . 7 7 7 7 7 7 1 1 7 7 7 . 
-    . . . . 7 6 7 7 7 7 7 7 7 7 . . 
-    . . . 7 7 7 7 7 7 7 2 7 7 7 7 7 
-    . . . 7 7 7 7 7 7 7 2 2 f f f 7 
-    . . 7 7 7 7 7 6 7 7 7 7 7 7 7 7 
-    . . 7 7 6 7 7 7 7 7 7 7 7 7 . . 
-    . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
-    . . 7 7 7 7 7 7 7 7 7 . . 7 . . 
-    . . 7 7 7 7 7 7 7 7 7 . . 7 7 . 
-    . . 7 7 7 7 7 7 7 7 7 . . . . . 
-    . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
-    . . . 7 7 7 7 . . . . 7 7 7 . . 
-    . . . . . . . . . . . . . 7 7 . 
-    `,img`
-    . . . . . . . 7 7 7 7 7 7 7 . . 
-    . . . . . 7 7 7 7 7 1 f 7 7 7 . 
-    . . . . 7 7 7 7 7 7 1 1 7 7 7 . 
-    . . . . 7 6 7 7 7 7 7 7 7 7 . . 
-    . . . 7 7 7 7 7 7 7 2 7 7 7 7 7 
-    . . . 7 7 7 7 7 7 7 2 2 f f f 7 
-    . . 7 7 7 7 7 6 7 7 7 7 7 7 7 7 
-    . . 7 7 6 7 7 7 7 7 7 7 7 7 . . 
-    . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
-    . . 7 7 7 7 7 7 7 7 7 . . 7 . . 
-    . . 7 7 7 7 7 7 7 7 7 . . 7 7 . 
-    . . 7 7 7 7 7 7 7 7 7 . . . . . 
-    . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
-    . . . 7 7 7 7 . . . . 7 7 7 . . 
-    . . . . . . . . . . . 7 7 7 . . 
-    . . . . . . . . . . . . . 7 7 . 
-    `],
-333,
-true
-)
-controller.moveSprite(mySprite, 120, 0)
-mySprite.ay = 500
-scene.cameraFollowSprite(mySprite)
+MakeMySprite()
 MakeLevel()
+MakePlatzBar()
+MakeVertPosBar()
+game.onUpdate(function () {
+    if (controller.left.isPressed()) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . 7 7 7 7 7 7 7 . . . . . . . 
+            . 7 7 7 f 1 7 7 7 7 7 . . . . . 
+            . 7 7 7 1 1 7 7 7 7 7 7 . . . . 
+            . . 7 7 7 7 7 7 7 7 6 7 . . . . 
+            7 7 7 7 7 2 7 7 7 7 7 7 7 . . . 
+            7 f f f 2 2 7 7 7 7 7 7 7 . . . 
+            7 7 7 7 7 7 7 7 6 7 7 7 7 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 6 7 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+            . . 7 . . 7 7 7 7 7 7 7 7 7 . . 
+            . 7 7 . . 7 7 7 7 7 7 7 7 7 . . 
+            . . . . . 7 7 7 7 7 7 7 7 7 . . 
+            . . . 7 7 7 7 7 7 7 7 7 7 7 . . 
+            . . 7 7 7 . . . . 7 7 7 7 . . . 
+            . 7 7 . . . . . . . . . . . . . 
+            `,img`
+            . . 7 7 7 7 7 7 7 . . . . . . . 
+            . 7 7 7 f 1 7 7 7 7 7 . . . . . 
+            . 7 7 7 1 1 7 7 7 7 7 7 . . . . 
+            . . 7 7 7 7 7 7 7 7 6 7 . . . . 
+            7 7 7 7 7 2 7 7 7 7 7 7 7 . . . 
+            7 f f f 2 2 7 7 7 7 7 7 7 . . . 
+            7 7 7 7 7 7 7 7 6 7 7 7 7 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 6 7 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+            . . 7 . . 7 7 7 7 7 7 7 7 7 . . 
+            . 7 7 . . 7 7 7 7 7 7 7 7 7 . . 
+            . . . . . 7 7 7 7 7 7 7 7 7 . . 
+            . . . 7 7 7 7 7 7 7 7 7 7 7 . . 
+            . . 7 7 7 . . . . 7 7 7 7 . . . 
+            . . 7 7 7 . . . . . . . . . . . 
+            . 7 7 . . . . . . . . . . . . . 
+            `],
+        333,
+        true
+        )
+    } else if (controller.right.isPressed()) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 7 7 7 7 7 7 7 . . 
+            . . . . . 7 7 7 7 7 1 f 7 7 7 . 
+            . . . . 7 7 7 7 7 7 1 1 7 7 7 . 
+            . . . . 7 6 7 7 7 7 7 7 7 7 . . 
+            . . . 7 7 7 7 7 7 7 2 7 7 7 7 7 
+            . . . 7 7 7 7 7 7 7 2 2 f f f 7 
+            . . 7 7 7 7 7 6 7 7 7 7 7 7 7 7 
+            . . 7 7 6 7 7 7 7 7 7 7 7 7 . . 
+            . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 . . 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 . . 7 7 . 
+            . . 7 7 7 7 7 7 7 7 7 . . . . . 
+            . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
+            . . . 7 7 7 7 . . . . 7 7 7 . . 
+            . . . . . . . . . . . . . 7 7 . 
+            `,img`
+            . . . . . . . 7 7 7 7 7 7 7 . . 
+            . . . . . 7 7 7 7 7 1 f 7 7 7 . 
+            . . . . 7 7 7 7 7 7 1 1 7 7 7 . 
+            . . . . 7 6 7 7 7 7 7 7 7 7 . . 
+            . . . 7 7 7 7 7 7 7 2 7 7 7 7 7 
+            . . . 7 7 7 7 7 7 7 2 2 f f f 7 
+            . . 7 7 7 7 7 6 7 7 7 7 7 7 7 7 
+            . . 7 7 6 7 7 7 7 7 7 7 7 7 . . 
+            . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 . . 7 . . 
+            . . 7 7 7 7 7 7 7 7 7 . . 7 7 . 
+            . . 7 7 7 7 7 7 7 7 7 . . . . . 
+            . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
+            . . . 7 7 7 7 . . . . 7 7 7 . . 
+            . . . . . . . . . . . 7 7 7 . . 
+            . . . . . . . . . . . . . 7 7 . 
+            `],
+        333,
+        true
+        )
+    }
+    vertBar.setLabel(convertToText(Math.round(mySprite.y)))
+})
